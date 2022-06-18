d71cbf0158703129bfc86b552b270d1f72c97a55
oxi <abdoumjr@gmail.com>
2022-06-18 12:04:18 +0100

    first commit

--- main.go
+++ main.go
@@ -8,6 +8,19 @@ type Number interface{
 
 
 func Sort[N Number](list []N) []N{
+
+	changed := true
+
+	for changed {
+		changed = false
+			for i:=0; i < len(list)-1; i++ {
+				if(list[i] > list[i+1]){
+					list[i], list[i+1] = list[i+1], list[i]
+					changed = true
+				}
+			}
+	}
+
 return list
 }
 
@@ -16,8 +29,7 @@ func main(){
 	 list1 := []int32{2,5,1,9,3,7,8,6,4,}
 	 list2 := []float32{0.5,0.3,0.1,0.9,0.7,0.8,0.6,0.4,0.2, 1.0,}
 
-	fmt.Println(Sort(list1))
-	fmt.Println(Sort(list2))
-
+	 fmt.Println("Sorted list1: ", Sort(list1))
+	 fmt.Println("Sorted list2: ", Sort(list2))
 
 }
